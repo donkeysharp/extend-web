7f7b8ca7cb9b941798b90d98bcfd3ae994dd2d79
Sergio Guillen <serguimant@gmail.com>
2015-08-14 17:19:17 -0400

    Fixed some general styles

--- app/views/widgets/sidebar.blade.php
+++ app/views/widgets/sidebar.blade.php
@@ -1,23 +1,38 @@
 <div id="sidebar-wrapper">
-  <div style="width: 100%; color: #eee; background-color: #2B3E3E; padding: 15px" class="clearfix">
+  <div class="clearfix sidebar-user">
     {{-- <img id="navbar-profile" src="{{ViewHelper::avatar(Auth::user()->picture_url)}}" style="width:50px; height: 50px; border: 1px solid #7D7D7D;" class="img-circle center-block"> --}}
     <h4 style="margin: 5px auto 0 auto;">{{Auth::user()->name}}</h4>
   </div>
   <ul class="sidebar-nav">
     <li>
-      <a href="{{url('dashboard')}}">Dashboard</a>
+      <a href="{{url('dashboard')}}">
+        <i class="fa fa-dashboard"></i>&nbsp;
+        Dashboard
+      </a>
     </li>
     <li>
-      <a href="{{url('dashboard/news')}}">Administrar Noticias</a>
+      <a href="{{url('dashboard/news')}}">
+        <i class="fa fa-newspaper-o"></i>&nbsp;
+        Administrar Noticias
+      </a>
     </li>
     <li>
-      <a href="{{url('dashboard/clients')}}">Administrar Clientes</a>
+      <a href="{{url('dashboard/clients')}}">
+        <i class="fa fa-exchange"></i>&nbsp;
+        Administrar Clientes
+      </a>
     </li>
     <li>
-      <a href="{{url('dashboard/media')}}">Administrar Medios</a>
+      <a href="{{url('dashboard/media')}}">
+        <i class="fa fa-video-camera"></i>&nbsp;
+        Administrar Medios
+      </a>
     </li>
     <li>
-      <a href="{{url('dashboard/topics')}}">Administrar Temas</a>
+      <a href="{{url('dashboard/topics')}}">
+        <i class="fa fa-folder-o"></i>&nbsp;
+        Administrar Temas
+      </a>
     </li>
   </ul>
 </div>
