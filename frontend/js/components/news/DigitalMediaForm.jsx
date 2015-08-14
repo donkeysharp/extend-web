b5609d901996c104662a0dcbde4f8364296fa725
Sergio Guillen <serguimant@gmail.com>
2015-08-13 21:21:12 -0400

    Added all media-form components

---
+++ frontend/js/components/news/DigitalMediaForm.jsx
@@ -0,0 +1,145 @@
+'use strict';
+var React = window.React;
+
+var DigitalMediaForm = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="section-divider">
+            <span>DIGITAL</span>
+          </div>
+          <div className="row">
+            <div className="col-md-5">
+              <select className="form-control">
+                <option id="123">--- Seleccion Medio ---</option>
+                <option id="123">meg bar </option>
+                <option id="123">meg bar </option>
+              </select>
+            </div>
+            <div className="col-md-1">
+              <button className="btn btn-light btn-add"><i className="fa fa-plus"></i></button>
+            </div>
+            <div className="col-md-3">
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-addon">
+                    <i className="fa fa-envelope"></i>
+                  </div>
+                  <input type="text" className="form-control" placeholder="Sección" />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-addon">
+                    <i className="fa fa-search"></i>
+                  </div>
+                  <input type="text" className="form-control" placeholder="Página" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="input-group">
+                <div className="input-group-addon">
+                  <i className="fa fa-envelope"></i>
+                </div>
+                <input type="text" className="form-control" placeholder="Título" />
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-7">
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-addon">
+                    <i className="fa fa-link"></i>
+                  </div>
+                  <input type="text" className="form-control" placeholder="Web" />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5">
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-addon">
+                    <i className="fa fa-user"></i>
+                  </div>
+                  <input type="text" className="form-control" placeholder="Género" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-5">
+              <select className="form-control">
+                <option id="123">--- Seleccione Tema ---</option>
+                <option id="123">meg bar </option>
+                <option id="123">meg bar </option>
+              </select>
+            </div>
+            <div className="col-md-1">
+              <button className="btn btn-light btn-add" title="Adicionar Tema">
+                <i className="fa fa-plus"></i>
+              </button>
+            </div>
+            <div className="col-md-3">
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-addon">
+                    <i className="fa fa-sliders"></i>
+                  </div>
+                  <input type="text" className="form-control" placeholder="Medida" />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-addon">
+                    <i className="fa fa-money"></i>
+                  </div>
+                  <input type="text" className="form-control" placeholder="Costo" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="clasification">
+                Tendencia&nbsp;&nbsp;&nbsp;
+                <label>
+                  <input type="radio" name="tendency" value="A" checked />
+                  Positiva
+                </label>
+                &nbsp;&nbsp;
+                <label>
+                  <input type="radio" name="tendency" value="B" />
+                  Negativa
+                </label>
+                &nbsp;&nbsp;
+                <label>
+                  <input type="radio" name="tendency" value="C" />
+                  Neutra
+                </label>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-12">
+              <textarea className="form-control" placeholder="Descripción" rows="5"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+module.exports = DigitalMediaForm;
--- frontend/js/components/news/GeneralFieldsEditor.jsx
+++ frontend/js/components/news/GeneralFieldsEditor.jsx
@@ -1,8 +1,35 @@
 'use strict';
 var React = window.React;
+var PrintedMediaForm = require('./PrintedMediaForm.jsx');
+var DigitalMediaForm = require('./DigitalMediaForm.jsx');
+var RadioMediaForm = require('./RadioMediaForm.jsx');
+var TvMediaForm = require('./TvMediaForm.jsx');
+var SourceMediaForm = require('./SourceMediaForm.jsx');
 
 function onSaveClick(e) {
   alert('foobar');
+}
+
+function getMediaForms() {
+  var mediaType = this.state.type;
+  var result = [];
+  if(mediaType.printed) {
+    result.push(<PrintedMediaForm />);
+  }
+  if (mediaType.digital) {
+    result.push(<DigitalMediaForm />);
+  }
+  if (mediaType.radio) {
+    result.push(<RadioMediaForm />);
+  }
+  if (mediaType.tv) {
+    result.push(<TvMediaForm />);
+  }
+  if (mediaType['source']) {
+    result.push(<SourceMediaForm />);
+  }
+
+  return result;
 }
 
 var GeneralFieldsEditor = React.createClass({
@@ -10,17 +37,17 @@ var GeneralFieldsEditor = React.createClass({
     return {
       mode: 'create',
       type: {
-        'printed': false,
-        'digital': false,
-        'radio': false,
-        'tv': false,
-        'source': false,
+        'printed': true,
+        'digital': true,
+        'radio': true,
+        'tv': true,
+        'source': true,
       }
     };
   },
-  render: function(){
+  render: function() {
     var buttonDisplay = this.state.mode === 'create' ? 'Continuar' : 'Guardar Noticia';
-
+    var mediaForms = getMediaForms.call(this);
     return (
       <div>
         <div className="row">
@@ -42,7 +69,7 @@ var GeneralFieldsEditor = React.createClass({
             </select>
           </div>
           <div className="col-md-1">
-            <button className="btn btn-light">
+            <button className="btn btn-light btn-add">
               <i className="fa fa-plus"></i>
             </button>
           </div>
@@ -146,126 +173,7 @@ var GeneralFieldsEditor = React.createClass({
             </label>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-12">
-            <div className="section-divider"><span>IMPRESO</span></div>
-            <div className="row">
-              <div className="col-md-5">
-                <select className="form-control">
-                  <option id="123">--- Seleccion Medio ---</option>
-                  <option id="123">meg bar </option>
-                  <option id="123">meg bar </option>
-                </select>
-              </div>
-              <div className="col-md-1">
-                <button className="btn btn-light"><i className="fa fa-plus"></i></button>
-              </div>
-              <div className="col-md-3">
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-addon">
-                      <i className="fa fa-envelope"></i>
-                    </div>
-                    <input type="text" className="form-control" placeholder="Sección" />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-addon">
-                      <i className="fa fa-search"></i>
-                    </div>
-                    <input type="text" className="form-control" placeholder="Página" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-addon">
-                      <i className="fa fa-user"></i>
-                    </div>
-                    <input type="text" className="form-control" placeholder="Título" />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-addon">
-                      <i className="fa fa-user"></i>
-                    </div>
-                    <input type="text" className="form-control" placeholder="Género" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-5">
-                <select className="form-control">
-                  <option id="123">--- Seleccione Tema ---</option>
-                  <option id="123">meg bar </option>
-                  <option id="123">meg bar </option>
-                </select>
-              </div>
-              <div className="col-md-1">
-                <button className="btn btn-light" title="Adicionar Tema">
-                  <i className="fa fa-plus"></i>
-                </button>
-              </div>
-              <div className="col-md-3">
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-addon">
-                      <i className="fa fa-sliders"></i>
-                    </div>
-                    <input type="text" className="form-control" placeholder="Medida" />
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-addon">
-                      <i className="fa fa-money"></i>
-                    </div>
-                    <input type="text" className="form-control" placeholder="Costo" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-12">
-                <div className="clasification">
-                  Tendencia&nbsp;&nbsp;&nbsp;
-                  <label>
-                    <input type="radio" name="tendency" value="A" checked />
-                    Positiva
-                  </label>
-                  &nbsp;&nbsp;
-                  <label>
-                    <input type="radio" name="tendency" value="B" />
-                    Negativa
-                  </label>
-                  &nbsp;&nbsp;
-                  <label>
-                    <input type="radio" name="tendency" value="C" />
-                    Neutra
-                  </label>
-                </div>
-              </div>
-            </div>
-            <br />
-            <div className="row">
-              <div className="col-md-12">
-                <textarea className="form-control" placeholder="Descripción" rows="5"></textarea>
-              </div>
-            </div>
-          </div>
-        </div>
+        {mediaForms}
         <br />
         <div className="row">
           <div className="col-md-4 col-md-offset-8">
