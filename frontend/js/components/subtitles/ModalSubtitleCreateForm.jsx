f16839b778ecd88a57499d42b4f5c2eab3bd5520
Sergio Guillen <serguimant@gmail.com>
2015-09-09 11:12:33 -0400

    Fixed bug when adding subtitle

--- frontend/js/components/subtitles/ModalSubtitleCreateForm.jsx
+++ frontend/js/components/subtitles/ModalSubtitleCreateForm.jsx
@@ -3,7 +3,7 @@ var React = window.React;
 var $http = require('../../http');
 
 function addSubtitle() {
-  if (this.refs.subtitle.getDOMNode().value) {
+  if (!this.refs.subtitle.getDOMNode().value) {
     return;
   }
   var data = {};
@@ -33,32 +33,32 @@ var ModalSubtitleCreateForm = React.createClass({
   render: function () {
     return (
       <div className="row">
-      <div className="col-md-4">
-      <div className="modal fade" ref="myModal" id={this.state.id} tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-      <div className="modal-dialog" role="document">
-      <div className="modal-content">
-      <div className="modal-header">
-      <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      <h4 className="modal-title" id="myModalLabel">Adicionar un subtítulo</h4>
-      </div>
-      <div className="modal-body">
-      <div className="form-group">
-      <label>Subtítulo</label>
-      <input className="form-control" placeholder="Introduzca subtítulo" ref="subtitle" />
-      </div>
-      </div>
-      <div className="modal-footer">
-      <a href="javascript:void(0)" data-dismiss="modal">Cancelar</a>
-      &nbsp;&nbsp;
-      <button onClick={addSubtitle.bind(this)} type="button" className="btn btn-light">
-      <i className="fa fa-save"></i>&nbsp;
-      Guardar Cambios
-      </button>
-      </div>
-      </div>
-      </div>
-      </div>
-      </div>
+        <div className="col-md-4">
+          <div className="modal fade" ref="myModal" id={this.state.id} tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h4 className="modal-title" id="myModalLabel">Adicionar un subtítulo</h4>
+                </div>
+                <div className="modal-body">
+                  <div className="form-group">
+                  <label>Subtítulo</label>
+                  <input className="form-control" placeholder="Introduzca subtítulo" ref="subtitle" />
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <a href="javascript:void(0)" data-dismiss="modal">Cancelar</a>
+                  &nbsp;&nbsp;
+                  <button onClick={addSubtitle.bind(this)} type="button" className="btn btn-light">
+                    <i className="fa fa-save"></i>&nbsp;
+                    Guardar Cambios
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       );
 }
