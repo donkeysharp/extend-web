2abd8d23e7c58996512aa19d64c510a447fe3f80
Sergio Guillen <serguimant@gmail.com>
2016-01-10 19:42:12 -0400

    [FIX] changed format and position for observations and extra title fields

--- app/views/bulletins/templates/news_display.blade.php
+++ app/views/bulletins/templates/news_display.blade.php
@@ -27,14 +27,17 @@
         <?php $displayed = true; ?>
       @endif
       @if($item->extra_title)
-        <h3 class="null" style="margin: 0;padding: 0;display: block;font-family: Helvetica;font-size: 24px;font-style: normal;font-weight: bold;line-height: 125%;letter-spacing: -.75px;text-align: left;color: #404040 !important;">
-        {{$item->extra_title}}
-        </h3>
+        <h3 style="margin: 0;padding: 0;display: block;font-family: Helvetica;font-style: normal;font-weight: normal;line-height: 125%;letter-spacing: -.75px;text-align: left;color: #404040 !important;">{{$item->extra_title}}</h3>
       @endif
       <h2 class="null" style="margin: 0;padding: 0;display: block;font-family: Helvetica;font-size: 26px;font-style: normal;font-weight: bold;line-height: 125%;letter-spacing: -.75px;text-align: left;color: #404040 !important;">
       {{$item->title}}
       </h2>
       <h3 style="margin: 0;padding: 0;display: block;font-family: Helvetica;font-style: normal;font-weight: normal;line-height: 125%;letter-spacing: -.75px;text-align: left;color: #404040 !important;">{{$item->media->name}}</h3>
+      @if($item->observations)
+      <p style="margin: 1em 0;padding: 0;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #9A9696;font-family: Helvetica;font-size: 15px;line-height: 150%;text-align: left;">
+        {{$item->observations}}
+      </p>
+      @endif
       <p style="margin: 1em 0;padding: 0;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #606060;font-family: Helvetica;font-size: 15px;line-height: 150%;text-align: left;">
       @if($firstPicture)
         <a href="{{asset('uploads/' . $firstPicture->file_name)}}" target="_blank">
@@ -43,11 +46,6 @@
       @endif
       {{$item->description}}
       </p>
-      @if($item->observations)
-      <p style="margin: 1em 0;padding: 0;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #606060;font-family: Helvetica;font-size: 12px;line-height: 150%;text-align: left;">
-        {{$item->observations}}
-      </p>
-      @endif
       @if($item->web)
         <a href="{{$item->web}}" target="_blank" style="color:#0082a4;font-family:Helvetica;sans-serif;font-size:14px">
           <img style="width: 25px; height: 25px;" src="{{asset('assets/img/bulletin/url.png')}}" height="25" width="25" >
